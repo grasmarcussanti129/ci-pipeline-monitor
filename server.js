792dee24a37c096fd15527d5ba007d2761c3bdf3
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-08-16 10:59:55 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+app.use(express.json());
+
+// Your MongoDB connection URL and configuration
+const mongoURI = 'mongodb://localhost:27017/cipipeline';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('CI Pipeline Monitor API');
+});
+
+// Socket.IO example event
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
